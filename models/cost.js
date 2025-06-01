4b9fc6bf9c54e4bebad69ce5422a77c6cef11278
LironGo <misterlir@gmail.com>
2025-06-01 18:17:46 +0300

    Add user and cost models

---
+++ models/cost.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const costSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['food', 'health', 'housing', 'sport', 'education'],
+    required: true
+  },
+  userid: {
+    type: Number,
+    required: true,
+    ref: 'User'
+  },
+  sum: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Cost', costSchema);
